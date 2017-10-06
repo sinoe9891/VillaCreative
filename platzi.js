458fa0449fa81aad9de462cbbadd865ded86374f
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2017-10-06 14:09:43 -0600

    RANDOM CON FOR

--- platzi.js
+++ platzi.js
@@ -1,6 +1,15 @@
+var z;
+
+for(var i=0; i<5; i++)
+{
+  z = aleatorio(10, 20);
+  document.write(z + ", ");
+
+}
+
 function aleatorio (min, maxi)
 {
   var resultado
-  resultado = Math.floor (Math.random() * (maxi - min +1)) + min;
-  return resultadp;
+  resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+  return resultado;
 }
